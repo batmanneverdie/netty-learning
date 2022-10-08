6206446f560808127fd7b4809ab56d11fb736df6
batmanneverdie <41139080+batmanneverdie@users.noreply.github.com>
2022-10-08 16:30:32 +0800

    adding discard protocol demo

--- src/main/java/com/laipan/demo/protocol/DiscardServerHandler.java
+++ src/main/java/com/laipan/demo/protocol/DiscardServerHandler.java
@@ -1,4 +1,31 @@
 package com.laipan.demo.protocol;
 
-public class DiscardServerHandler {
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+import java.nio.charset.StandardCharsets;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            // 释放打印后的接收 msg
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
